e46b9fa92c7dca45f5945536dea5bcae670102f5
daisukeokamoto1023 <okadaih11023@gmail.com>
2020-04-30 00:15:58 +0900

    add unit test Timeutilclass using pytest

--- src/scraping.py
+++ src/scraping.py
@@ -1,30 +1,34 @@
 import requests
 import re
-import os
 
 from bs4 import BeautifulSoup
 
 from src.lib.common_util import TimeUtil
 
+
 class Scraper:
     def __init__(self, base):
         self.base = base
         self.time_util = TimeUtil()
+
     def getTargetUrl(self, base_url, target_url):
         r = requests.get(self.base + base_url)
         r.encoding = r.apparent_encoding
         soup = BeautifulSoup(r.text, 'html.parser')
         elems = soup.find(href=re.compile(target_url))
         return elems.get('href')
+
     def getContent(self, url):
         r = requests.get(self.base + url)
         r.encoding = r.apparent_encoding
         return BeautifulSoup(r.text, 'html.parser')
+
     def downloadPdf(self, pdf_url, path_name):
         r = requests.get(self.base + pdf_url, stream=True)
         with open(path_name, 'wb') as fd:
             for chunk in r.iter_content(chunk_size=2000):
                 fd.write(chunk)
+
     def parseSingleTable(self, soup_data):
         table = soup_data.findAll('table')[0]
         numbers = table.findAll("th")[1:]
@@ -32,50 +36,56 @@ class Scraper:
         dataset = []
         for number, row in zip(numbers, rows):
             day_proved, tmp = row.findAll("td")
-            #dayにpタグが付いているパターンがある
+            # dayにpタグが付いているパターンがある
             correct_date = tmp.findAll('a')[-1]
             day_release = correct_date.getText()
             ad_day_release = self.time_util.executeConvert(day_release.strip())
-            ad_day_proved = self.time_util.executeConvert(day_proved.text.strip())
+            ad_day_proved = self.time_util.executeConvert(
+                day_proved.text.strip())
 
             link = correct_date.get('href')
 
-            dataset.append([number.find('p').text, ad_day_release, ad_day_proved, link])       
+            dataset.append(
+                [number.find('p').text, ad_day_release, ad_day_proved, link])
         return dataset
+
     def findAllTable(self, soup_data):
         return soup_data.findAll('table')
+
     def parseContactsTable(self, table):
         def convertDaysCountKV(items):
             result = []
             for item in items:
-                #誤字で日が件と表示されている箇所があるため、日と件の両方で分割
+                # 誤字で日が件と表示されている箇所があるため、日と件の両方で分割
                 key, count, *_ = re.split(r'日|件', item)
-                result.append({'day':int(key), 'count':int(count)})
+                result.append({'day': int(key), 'count': int(count)})
             return result
 
-        result_list = []
         items = table.findAll('tr')[1:]
-        res_dict =[]
+        res_dict = []
         for item in items:
-            #span
-            #td属性からテキストを取得
+            # span
+            # td属性からテキストを取得
             span, sum_number_tmp, individuals_tmp = item.findAll('td')
             sum_number = sum_number_tmp.findAll('p')[0]
-            individuals = list(individuals_tmp.findAll('p')[0].text.strip().split('、'))
-            
-            #spanからreturnするdictonaryのlistを取得する
+            individuals = list(individuals_tmp.findAll('p')
+                               [0].text.strip().split('、'))
+
+            # spanからreturnするdictonaryのlistを取得する
             target_list = TimeUtil().getDatetimeDictFromString(span.text.strip())
 
-            #日ごとの件数をkvで取得
+            # 日ごとの件数をkvで取得
             days_count_dict = convertDaysCountKV(individuals)
 
             for day_count in days_count_dict:
-    
-                target_list = list(map(lambda x:{'日付':x['日付'], '小計': day_count['count'] if x['day'] == day_count['day'] else x['小計'], 'day':x['day']},target_list))
+
+                target_list = list(map(lambda x: {
+                                   '日付': x['日付'], '小計': day_count['count'] if x['day'] == day_count['day'] else x['小計'], 'day': x['day']}, target_list))
 
             res_dict.extend(target_list)
-        #一次変数の'day'を消して返却
-        return list(map(lambda x:{'日付':x['日付'], '小計':x['小計']}, res_dict))
+        # 一次変数の'day'を消して返却
+        return list(map(lambda x: {'日付': x['日付'], '小計': x['小計']}, res_dict))
+
 
 if __name__ == '__main__':
     print("main")

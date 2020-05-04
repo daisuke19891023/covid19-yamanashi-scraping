199ab3a5e2dfca27e31dec817cbea9a21a745fd3
daisukeokamoto1023 <okadaih11023@gmail.com>
2020-05-04 12:37:07 +0900

    exclude zero max date data

--- src/statics.py
+++ src/statics.py
@@ -1,3 +1,6 @@
+from src.lib.json_checker import JsonChecker
+
+
 def getStaticsDataDict(scr, table, comments, update_datetime):
     res_dict = scr.parseContactsTable(table)
 
@@ -5,5 +8,11 @@ def getStaticsDataDict(scr, table, comments, update_datetime):
     dictonary = {}
     dictonary['__comments'] = comments
     dictonary['date'] = update_datetime
-    dictonary['data'] = sorted(res_dict, key=lambda x: x['日付'])
+
+    # 小計が0とならない最新の日付までのリストにする
+    jc = JsonChecker()
+    data = sorted(res_dict, key=lambda x: x['日付'])
+    data = jc.exclude_zero_max_date(data)
+    dictonary['data'] = data
+
     return dictonary

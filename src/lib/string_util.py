50c86ec3bf6b70df10e7d46beb336f26a1def12f
daisukeokamoto1023 <okadaih11023@gmail.com>
2020-06-14 16:15:50 +0900

    modify scraping way of patient data. before 60, get from pdf. after 61, get from html.

--- src/lib/string_util.py
+++ src/lib/string_util.py
@@ -5,7 +5,7 @@ from typing import List, Tuple, Union
 class StringUtil:
     def __init__(self):
         super().__init__()
-        self.exclude_char = r'県外|再陽性'
+        self.exclude_char = r'県外|再陽性|空港検疫で確認'
 
     def exclude_outside(self, full_with_str: str) -> bool:
         if re.search(self.exclude_char, full_with_str):
@@ -20,3 +20,10 @@ class StringUtil:
         else:
             tmp = re.sub(r'県|内|例|目', '', number_char)
             return True, list(map(lambda x: '県内{}例目'.format(x), tmp.split(',')))
+
+    def exclude_info_number(self, number_char: str) -> str:
+        m = re.search(r'県内(\d{1,3})例目', number_char)
+        if m is not None:
+            return m.group()
+        else:
+            return number_char
--- src/patient.py
+++ src/patient.py
@@ -16,6 +16,8 @@ from src.lib.json_checker import JsonChecker
 class Patient:
     def __init__(self, update_datetime):
 
+        self.patient_list = []
+
         self.patients = {}
         self.patients['__comments'] = "陽性患者の属性"
         self.patients['date'] = update_datetime
@@ -40,7 +42,7 @@ class Patient:
         # 県外事例は除外
         inside_checker = StringUtil()
         patient_data_tmp = []
-        patients_summary_tmp = []
+
         for data in dataset:
             if inside_checker.exclude_outside(data[0]):
 
@@ -58,15 +60,13 @@ class Patient:
                     for n in number_char:
                         # print('No:{} リリース日:{} 判明日:{} Link:{}'.format(n, data[1].strip(), data[2].strip(), output_path))
                         patient_data_tmp.append(
-                            {"No": n, "リリース日": data[1].strip(), "link": output_path})
-                        patients_summary_tmp.append(data[2].strip())
+                            {"No": n, "リリース日": data[1].strip(), "判明日": data[2].strip(), "link": output_path})
 
                 # 単一の場合
                 else:
                     # print('No:{} リリース日:{} 判明日:{} Link:{}'.format(number_char, data[1].strip(), data[2].strip(), output_path))
                     patient_data_tmp.append(
-                        {"No": number_char, "リリース日": data[1].strip(), "link": output_path})
-                    patients_summary_tmp.append(data[2].strip())
+                        {"No": number_char, "リリース日": data[1].strip(), "判明日": data[2].strip(), "link": output_path})
 
         # convertの実行
         path_op.create_path('text')
@@ -75,7 +75,6 @@ class Patient:
 
         # テキストからjsonの作成
         parser = TextParser()
-        patient_list = []
 
         # patientの作成
         for tmp in patient_data_tmp:
@@ -83,25 +82,28 @@ class Patient:
             result.update(tmp)
             result.update({'退院': None})
             del result['link']
-            patient_list.append(result)
+            self.patient_list.append(result)
 
         # Noに数字が入らない場合の処理（例："再陽性"）
         nan_patient_list = list(filter(lambda x: re.search(
-            r'\d', x['No']) is None, patient_list))
+            r'\d', x['No']) is None, self.patient_list))
         number_patient_list = list(filter(lambda x: re.search(
-            r'\d', x['No']) is not None, patient_list))
+            r'\d', x['No']) is not None, self.patient_list))
         insert_patient_list = sorted(number_patient_list, key=lambda x: int(
             re.sub(r'県|内|例|目', '', x['No'])))
         insert_patient_list.extend(nan_patient_list)
         self.patients['data'] = insert_patient_list
 
         # patients_summaryの作成
-        self.create_patients_summary_dict(patients_summary_tmp)
+        self.create_patients_summary_dict(insert_patient_list)
         return self.patients, self.patients_summary_data
 
-    def create_patients_summary_dict(self, patients_summary_tmp):
+    def create_patients_summary_dict(self, patient_list):
         # patients_summaryの作成
         patients_summary = TimeUtil().create_dt_dict(datetime.datetime.now())
+        # patient_listから"判明日"のみのリストを作成
+        patients_summary_tmp = list(map(lambda x: x["判明日"], patient_list))
+
         for k, g in groupby(patients_summary_tmp):
             patients_summary = list(map(lambda x: {"日付": x["日付"], "小計": len(
                 list(g)) if x['日付'] == k else x['小計']}, patients_summary))
@@ -119,28 +121,47 @@ class Patient:
         print(data)
         patients = []
         patient = {}
-        patient["No"] = data.text
-        pattern = re.compile(r'発生判明日：')
+        patient["No"] = StringUtil(
+        ).exclude_info_number(data.text)
+        pattern = r'年代|性別|判明日|居住地'
+
         for index, sibling in enumerate(data.next_siblings):
             if index % 2 != 0:  # 改行コードはスキップする
                 # h2属性の場合、新たなpatient dictを
                 if sibling.name == 'h4':
+                    patient["退院"] = None
+                    if patient["判明日"] is not None:
+                        patient["判明日"] = TimeUtil().convert_wareki_to_ad(
+                            patient["判明日"])
+                        patient["リリース日"] = patient["判明日"]
                     patients.append(patient)
                     patient = {}
-                    patient["No"] = sibling.text
+                    patient["No"] = StringUtil(
+                    ).exclude_info_number(sibling.text)
                     continue
 
-                # 判明日
-                if re.match(pattern, sibling.text):
-                    patient["判明日"] = re.sub(pattern, '', sibling.text)
+                text = re.sub(r':|︓|：', '', sibling.text)
+                m = re.search(pattern, text)
+                # print(text)
+                if m is not None:
+                    key = m.group()
+                    value = text[m.end():]
+                    # 年代の表記ゆれの統一（歳代→代）
+                    if key == '年代':
+                        value = re.sub(r'歳', '', value)
+                    # keyが存在しない場合のみ代入する
+                    if key not in patient:
+                        patient[key] = value
                     continue
-
-                # print(repr(sibling))
-                # print(type(repr(sibling)))
-                # print(type(sibling))
+            # h2タグが表示された時点で別と患者情報の表示は終了する
             if sibling.name == 'h2':
+                patient["退院"] = None
+                if patient["判明日"] is not None:
+                    patient["判明日"] = TimeUtil().convert_wareki_to_ad(
+                        patient["判明日"])
+                    patient["リリース日"] = patient["判明日"]
                 patients.append(patient)
                 break
         print(patients)
-
-        # print(sibling.find('p'))
+        self.patient_list = list(filter(lambda x: re.search(
+            r'\d', x['No']) is not None, patients))
